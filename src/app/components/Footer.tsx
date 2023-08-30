edff0dfbda6d9bbbbb398c84e89f3aa2aca43098
Siravitt <siravitt.tan@gmail.com>
2023-08-30 22:32:15 +0700

    :sparkles: :art: :iphone: navbar now link to other pages, separate each page and improve layout, improve responsive for all display

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,22 +1,23 @@
+import Link from 'next/link';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import FacebookIcon from '@mui/icons-material/Facebook';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import PhoneIcon from '@mui/icons-material/Phone';
 
 export default function Footer() {
     return (
-        <div className="w-full h-14 grid grid-cols-2 absolute bottom-0 border-t text-light-primary md:grid-cols-6 xl:grid-cols-12">
+        <div className="w-full h-14 grid grid-cols-2 border-t text-light-primary md:grid-cols-6 xl:grid-cols-12">
             <div className="grid place-content-center border-r">find me in :</div>
             <div className="grid grid-cols-3 md:flex">
-                <div className="grid place-content-center border-r md:w-[60px]"><PhoneIcon fontSize='large' /></div>
-                <div className="grid place-content-center border-r md:w-[60px]"><FacebookIcon fontSize='large' /></div>
-                <div className=" grid place-content-center md:hidden"><GitHubIcon fontSize='large' /></div>
+                <button className="grid place-content-center border-r md:w-[60px] hover:text-white duration-150"><PhoneIcon fontSize='large' /></button>
+                <Link href={''} target='_blank' className="grid place-content-center border-r md:w-[60px] hover:text-white duration-150"><LinkedInIcon fontSize='large' /></Link>
+                <Link href={'https://github.com/Siravitt'} target='_blank' className=" grid place-content-center md:hidden"><GitHubIcon fontSize='large' /></Link>
             </div>
-            <div className="hidden border-l md:grid md:place-content-center md:col-start-6 xl:col-start-12">
+            <Link href={'https://github.com/Siravitt'} target='_blank' className="hidden md:border-l md:grid md:place-content-center md:col-start-6 xl:col-start-12 hover:text-white hover:text-lg duration-150">
                 <div className='flex items-center gap-2'>
                     <span>@Siravitt</span>
-                    <GitHubIcon fontSize='large' />
+                    <GitHubIcon fontSize="large" />
                 </div>
-            </div>
+            </Link>
         </div>
     )
 }
