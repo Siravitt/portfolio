5f0f30d02806980072a440420e9941334eab2e64
Siravit Tanratvijit <siravittanratvijit@Siravits-MacBook-Air.local>
2023-08-31 13:06:48 +0700

    :iphone: fix responsive

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,15 +5,15 @@ import PhoneIcon from '@mui/icons-material/Phone';
 
 export default function Footer() {
     return (
-        <div className="w-full h-14 grid grid-cols-2 border-t text-light-primary md:grid-cols-6 xl:grid-cols-12">
-            <div className="grid place-content-center border-r">find me in :</div>
-            <div className="grid grid-cols-3 md:flex">
+        <div className="w-full h-14 grid grid-cols-2 border-t text-light-primary md:grid-cols-12 xl:grid-cols-12">
+            <div className="grid place-content-center border-r md:col-span-2">find me in :</div>
+            <div className="grid grid-cols-3 md:flex md:col-span-4">
                 <button className="grid place-content-center border-r md:w-[60px] hover:text-white duration-150"><PhoneIcon fontSize='large' /></button>
                 <Link href={''} target='_blank' className="grid place-content-center border-r md:w-[60px] hover:text-white duration-150"><LinkedInIcon fontSize='large' /></Link>
                 <Link href={'https://github.com/Siravitt'} target='_blank' className=" grid place-content-center md:hidden"><GitHubIcon fontSize='large' /></Link>
             </div>
-            <Link href={'https://github.com/Siravitt'} target='_blank' className="hidden md:border-l md:grid md:place-content-center md:col-start-6 xl:col-start-12 hover:text-white hover:text-lg duration-150">
-                <div className='flex items-center gap-2'>
+            <Link href={'https://github.com/Siravitt'} target='_blank' className='hidden md:block md:border-l md:col-span-3 md:col-start-10 xl:col-span-2 xl:col-start-11 hover:text-white hover:text-lg duration-150'>
+                <div className='flex justify-center items-center gap-2 h-full object-contain'>
                     <span>@Siravitt</span>
                     <GitHubIcon fontSize="large" />
                 </div>
--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -11,8 +11,8 @@ export default function NavBar() {
             <div className="grid col-span-8 place-items-center md:border-r md:place-content-center md:col-span-4 xl:col-span-2">siravit-tanratvijit</div>
             <Link href={'/hello'} className={`hidden place-content-center ${pathname === "/hello" ? "text-white border-b-2 border-b-plum" : ""} md:border-r md:grid md:col-span-2 xl:col-span-1 hover:text-white hover:text-lg duration-150`}>_hello</Link>
             <Link href={'/about-me'} className={`hidden place-content-center ${pathname === "/about-me" ? "text-white border-b-2 border-b-plum" : ""} md:border-r md:grid md:col-span-2 xl:col-span-1 hover:text-white hover:text-lg duration-150`}>_about-me</Link>
-            <Link href={'/projects'} className={`hidden place-content-center ${pathname === "/projects" ? "text-white border-b-2 border-b-plum" : ""} md:border-r md:grid md:col-span-2 xl:col-span-1 hover:text-white hover:text-lg duration-150`}>_projects</Link>
-            <Link href={'/contact-me'} className={`hidden place-content-center ${pathname === "/contact-me" ? "text-white border-b-2 border-b-plum" : ""} md:border-l md:grid md:col-span-2 xl:col-span-1 xl:col-start-12 hover:text-white hover:text-lg duration-150`}>_contact-me</Link>
+            <Link href={'/projects'} className={`hidden place-content-center ${pathname === "/projects" ? "text-white border-b-2 border-b-plum" : ""} xl:border-r md:grid md:col-span-2 xl:col-span-1 hover:text-white hover:text-lg duration-150`}>_projects</Link>
+            <Link href={'/contact-me'} className={`hidden place-content-center ${pathname === "/contact-me" ? "text-white border-b-2 border-b-plum" : ""} md:border-l md:grid md:col-span-2 xl:col-span-2 xl:col-start-11 hover:text-white hover:text-lg duration-150`}>_contact-me</Link>
             <div className="grid col-span-2 place-content-center md:hidden"><MenuRoundedIcon fontSize='large' /></div>
         </div>
     );
